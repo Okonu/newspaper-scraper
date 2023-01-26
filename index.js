c4949833d5ca4d0f8406915bdff716fdae081873
Okonu <ianyakundi015@gmail.com>
2023-01-26 15:44:57 +0300

    Scraping news articles related to climate crisis from multiple newspapers and pushing them to an array with source

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio')
 
 const app = express()
 
-const articles = [
+const newspapers = [
     {
         name: 'thetimes',
         address: 'https://www.thetimes.co.uk/environment/climate-change'
@@ -16,15 +16,36 @@ const articles = [
     },
     {
         name: 'the telegraph',
-        address: 'https://www.telegraph.co.uk/climate-change
+        address: 'https://www.telegraph.co.uk/climate-change'
     }
 ]
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")',html).each(function(){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title,
+                url,
+                source: newspaper.name
+            })
+        })
+    })
+})
 
 app.get('/', (req, res ) =>{
     res.json('These are the latest Climate Change News around the World')
 })
-
-// app.get('/news', (req, res) =>{
+ 
+app.get('/news', (req, res) =>{
+    res.json(articles)
 //     axios.get('https://www.theguardian.com/environment/climate-crisis')
 //     .then((response) =>{
 //         const html = response.data
@@ -39,6 +60,6 @@ app.get('/', (req, res ) =>{
 //         }) 
 //         res.json(articles)
 //     }).catch((err) => console.log(err))
-// })
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`)) 
