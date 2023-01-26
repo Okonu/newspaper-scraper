8552bdfdeb710c77811b6ec66ffdd73ce07dcc2d
Okonu <ianyakundi015@gmail.com>
2023-01-26 16:47:36 +0300

    Add route handler for fetching articles from specified newspaper by ID

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = process.env.PORT ||8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -64,5 +64,33 @@ app.get('/news', (req, res) =>{
 //         res.json(articles)
 //     }).catch((err) => console.log(err))
 })
+
+
+//route to get a specific article
+app.get('/news/:newspaperId', async (req, res) =>{
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].base
+
+    axios.get(newspaperAddress)
+        .then(response =>{
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function (){
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`)) 
