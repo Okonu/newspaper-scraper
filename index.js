80514c41cadd712f18ecce95ff111ae1cfbde325
Okonu <ianyakundi015@gmail.com>
2023-01-26 15:20:02 +0300

    Scraping news articles related to climate crisis from The Guardian website and pushing them to an arra

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+const articles = []
+
+app.get('/', (req, res ) =>{
+    res.json('These are the latest Climate Change News around the World')
+})
+
+app.get('/news', (req, res) =>{
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) =>{
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function(){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title, url
+            })
+        }) 
+        res.json(articles)
+    }).catch((err) => console.log(err))
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`)) 
